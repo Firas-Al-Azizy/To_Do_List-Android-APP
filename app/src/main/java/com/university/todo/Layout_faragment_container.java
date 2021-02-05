5fb9d6879ff5539697064b2db6be73de0161cec4
Heba Fayad <43901699+HebaFayad@users.noreply.github.com>
2021-02-05 18:01:51 +0300

    adding Layout_faragment_container

---
+++ app/src/main/java/com/university/todo/Layout_faragment_container.java
@@ -0,0 +1,14 @@
+package com.university.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Layout_faragment_container extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_faragment_container);
+    }
+}
