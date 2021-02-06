cb16ad11f604809f699366bd838cb5497a1d19e0
Heba Fayad <43901699+HebaFayad@users.noreply.github.com>
2021-02-06 19:34:41 +0300

    update main activity and input activity

--- app/src/main/java/com/university/todo/MainActivity.java
+++ app/src/main/java/com/university/todo/MainActivity.java
@@ -1,37 +1,32 @@
 package com.university.todo;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-   // private RecyclerView recyclerView;
+    // private RecyclerView recyclerView;
     private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       
+
         //recyclerView=findViewById(R.id.recycl_view);
-        floatingActionButton=findViewById(R.id.floatingActionButton);
+        floatingActionButton = findViewById(R.id.floatingActionButton);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent= new Intent(MainActivity.this,InputActivity.class);
+                Intent intent = new Intent(MainActivity.this, InputActivity.class);
                 startActivity(intent);
             }
         });
 
 //      
+    }
 }
