db5542fd629a50aa00357748934e92f7bcdfa2a2
Heba Fayad <43901699+HebaFayad@users.noreply.github.com>
2021-02-06 10:39:21 +0300

    adding style , strings.xml , Utilities, AddToDoActivity.java, some drawables

---
+++ app/src/main/java/com/university/todo/AddToDoActivity.java
@@ -0,0 +1,14 @@
+package com.university.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddToDoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_to_do);
+    }
+}
